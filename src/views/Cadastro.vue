76d178dfe80ea4a3c58e22e6946f747e3ad75383
Thiago Draeger <thiagodraeger123@gmail.com>
2022-09-08 13:50:32 -0300

    perfil

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -1,96 +1,134 @@
 <template>
-    <v-container fill-height fluid text-center>
-      <v-container class="cont">
-        <v-row>
-            <v-col class="mx-auto pb-1 d-flex justify-center">
-                <h1>Cadastre-se aqui:</h1>
-            </v-col>
-        </v-row>
-        <v-row>
-          <v-col d-flex>
-            <v-alert class="box">
-              <v-form style="width:45rem">
-                <v-text-field
-                  flat
-                  class="input-field mt-5 mb-5"
-                  label="Nome"
-                  v-model="user.Nome"
-                  @keyup.enter="login"
-                  hide-details="auto"
-                >
-                </v-text-field>
-                <v-text-field
-                  flat
-                  class="input-field mt-5 mb-5"
-                  label="Email"
-                  v-model="user.email"
-                  @keyup.enter="login"
-                  hide-details="auto"
-                >
-                </v-text-field>
-                <v-text-field
-                  flat
-                  class="input-field mt-5 mb-5"
-                  hide-details="auto"
-                  label="Senha"
-                  v-model="user.password"
-                  :type="show ? 'text' : 'password'"
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="show = !show"
-                  @keyup.enter="login"
-                ></v-text-field>
-                <v-text-field
-                  flat
-                  class="input-field mt-5 mb-5"
-                  hide-details="auto"
-                  label="Confirmar Senha"
-                  v-model="user.cpassword"
-                  :type="show ? 'text' : 'password'"
-                  :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                  @click:append="show = !show"
-                  @keyup.enter="login"
-                ></v-text-field>
-                <v-btn color="green" @click="login"> Login</v-btn>
-                <v-btn class="ml-4" color="red" @click="reset"> Cancelar</v-btn>
-              </v-form>
-            </v-alert>
+  <v-container fill-height fluid text-center>
+    <v-container class="cont">
+      <v-row>
+          <v-col class="mx-auto pb-1 d-flex justify-center">
+              <h1>Cadastre-se aqui:</h1>
           </v-col>
-        </v-row>
-      </v-container>
+      </v-row>
+      <v-row>
+        <v-col d-flex>
+          <v-alert class="box" white>
+            <v-form style="width:45rem">
+              <v-text-field
+                rounded  
+                class="input-field mt-5 mb-5"
+                solo
+                placeholder="Nome"
+                label="Nome"
+                v-model="user.Nome"
+                @keyup.enter="login"
+                hide-details="auto"
+              >
+              </v-text-field>
+              <v-text-field
+                rounded  
+                class="input-field mt-5 mb-5"
+                solo
+                placeholder="Email"
+                label="Email"
+                v-model="user.email"
+                @keyup.enter="login"
+                hide-details="auto"
+              >
+              </v-text-field>
+              <v-text-field
+                rounded
+                solo
+                placeholder="Senha"
+                class="input-field mt-5 mb-5"
+                hide-details="auto"
+                label="Senha"
+                v-model="user.password"
+                :type="show ? 'text' : 'password'"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="show = !show"
+                @keyup.enter="login"
+              ></v-text-field>
+              <v-text-field
+                rounded
+                solo
+                placeholder="Confirmar Senha"
+                class="input-field mt-5 mb-5"
+                hide-details="auto"
+                label="Confirmar Senha"
+                v-model="user.cpassword"
+                :type="show ? 'text' : 'password'"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="show = !show"
+                @keyup.enter="login"
+              ></v-text-field>
+              <v-btn color="grey" @click="login"> Login</v-btn>
+            </v-form>
+          </v-alert>
+          <v-card class="transparent" dark>
+            <v-card-text>
+              <v-row class="font-italic pb-1" justify="center">
+                <a href="/">Já tem uma conta? Entre</a>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        user: {},
-        show: false,
-        errorLogin: false,
-        novaConta: false,
-      };
-    },
-  };
-  </script>
-  
-  <style scoped> 
+  </v-container>
+</template>
 
-  .cont{
-    background-color: white;
-    width: 47rem;
-    border-radius: 30px; 
-  }
+<script>
+export default {
+  data() {
+    return {
+      user: {},
+      show: false,
+      errorLogin: false,
+      novaConta: false,
+    };
+  },
+};
+</script>
 
-  .box{
-    background-color:transparent;
-    padding:0;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-content: center;
-  }
-  .input-field{
-    margin: 2%;
-  }
-  </style>
+<style scoped>
+a:link {
+  color: white;
+  text-decoration: none;
+  font-size: 20px;
+}
+
+a:visited {
+  color: white;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+h1{
+  color: white;
+  }  
+.cont{
+  width: 47rem;
+  border-radius: 30px; 
+}
+.box{
+  background-color:transparent;
+  padding:0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
   
+}
+.input-field{
+  border-radius: 40px;
+  margin: 2%;
+  background-color:white;
+
+}
+textarea:focus, input:focus{
+    outline: none;
+}
+.text-fieldct{
+  width: 100%;
+}
+</style>
--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -6,24 +6,26 @@
       <v-container class="mt-4">
         <v-col>
           <v-row>
-            <v-img max-height="120" max-width="120"
+            <v-img max-height="300" max-width="300"
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"
             class="mb-5"
-            style="border-radius: 15px"
+            style="border-radius: 1000px"
             >
             </v-img>
+            <v-col>
+              <v-text-field label="Nome" dark v-model="nome" outlined dense></v-text-field>
+              <v-text-field label="Email" dark v-model="email" outlined dense></v-text-field>
+              <v-textarea outlined dense>
+              <template v-slot:label>
+                <div>
+                  Descrição
+                </div>
+              </template>
+              </v-textarea>
+              <v-btn color="blue" @click="salvarPerfil"> Salvar</v-btn>
+            </v-col>
           </v-row>
         </v-col>
-        <v-text-field label="Nome" dark v-model="nome" outlined dense></v-text-field>
-        <v-text-field label="Email" dark v-model="email" outlined dense></v-text-field>
-        <v-textarea outlined dense>
-          <template v-slot:label>
-            <div>
-              Descrição
-            </div>
-          </template>
-        </v-textarea>
-        <v-btn color="blue" @click="salvarPerfil"> Salvar</v-btn>
       </v-container>
     </v-form>
   </v-card>
