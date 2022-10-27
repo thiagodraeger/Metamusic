502435a20c88ac9299f95301ae0cd0507b38d5a5
Thiago Draeger <thiagodraeger123@gmail.com>
2022-10-27 10:29:47 -0300

    cadastro

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -2,22 +2,21 @@
   <v-container fill-height fluid text-center>
     <v-container>
       <v-row>
-          <v-col class="mx-auto pb-1 d-flex justify-center">
-              <h1>Cadastre-se aqui:</h1>
-          </v-col>
+        <v-col class="mx-auto pb-1 d-flex justify-center">
+          <h1>Cadastre-se aqui:</h1>
+        </v-col>
       </v-row>
       <v-row>
         <v-col d-flex>
           <v-alert class="box" white>
-            <v-form style="width:50vw">
+            <v-form style="width: 50vw">
               <v-text-field
-                rounded  
+                rounded
                 class="input-field mt-5 mb-5"
                 solo
                 placeholder="Nome"
                 label="Nome"
-                v-model="user.Nome"
-                @keyup.enter="novaConta"
+                v-model="user.username"
                 hide-details="auto"
               >
               </v-text-field>
@@ -28,7 +27,6 @@
                 placeholder="Email"
                 label="Email"
                 v-model="user.email"
-                @keyup.enter="novaConta"
                 hide-details="auto"
               >
               </v-text-field>
@@ -43,9 +41,14 @@
                 :type="show ? 'text' : 'password'"
                 :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
                 @click:append="show = !show"
-                @keyup.enter="novaConta"
-              ></v-text-field>
-              <v-btn color="grey" @click="Cadastrar" href="/home" style="font-size: 15px; color: black;   text-decoration: none;">Cadastrar </v-btn>
+              >
+              </v-text-field>
+              <v-btn
+                color="grey"
+                @click="register"
+                style="font-size: 15px; color: black; text-decoration: none"
+                >Cadastrar
+              </v-btn>
             </v-form>
           </v-alert>
           <v-card class="transparent" dark>
@@ -62,14 +65,23 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       user: {},
-      show: false,
-      errorLogin: false,
-      novaConta: false,
     };
+  },
+  methods: {
+    async register() {
+      try {
+        await axios.post("register/", this.user);
+        this.$router.push("/");
+      } catch (e) {
+        console.log(e);
+        alert("algo de errado não está certo.");
+      }
+    },
   },
 };
 </script>
@@ -90,28 +102,27 @@ a:hover {
   text-decoration: underline;
 }
 
-h1{
+h1 {
   color: white;
-  }  
-.box{
-  background-color:transparent;
-  padding:0;
+}
+.box {
+  background-color: transparent;
+  padding: 0;
   width: 100%;
   display: flex;
   justify-content: center;
   align-content: center;
-  
 }
-.input-field{
+.input-field {
   border-radius: 40px;
   margin: 2%;
-  background-color:white;
-
+  background-color: white;
 }
-textarea:focus, input:focus{
-    outline: none;
+textarea:focus,
+input:focus {
+  outline: none;
 }
-.text-fieldct{
+.text-fieldct {
   width: 100%;
 }
 </style>
