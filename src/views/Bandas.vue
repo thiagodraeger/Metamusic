8b93283c5bec5b844fc34091f83bb0fb0d428e9f
Thiago Draeger <thiagodraeger123@gmail.com>
2022-10-27 10:26:51 -0300

    capa banda

--- src/views/Bandas.vue
+++ src/views/Bandas.vue
@@ -6,17 +6,15 @@
         <v-col v-for="banda in bandas" :key="banda.id" cols="3">
           <v-card link :to="`/artistas/${banda.id}`">
             <v-img
-              :src="imagemFixa"
+              :src="banda.foto ? banda.foto.url : null"
               class="white--text align-end"
               gradient="to bottom, rgba(0,0,0,.3), rgba(0,0,0,.5)"
               height="250px"
-              
             >
               <v-card-title v-text="banda.nome_banda"></v-card-title>
               <!-- <v-card-subtitle v-text="banda.desc_banda"></v-card-subtitle> -->
               <v-card-subtitle v-text="banda.ano_criacao"></v-card-subtitle>
             </v-img>
-
           </v-card>
         </v-col>
       </v-row>
@@ -25,20 +23,19 @@
 </template>
 
 <script>
-import BandaService from '@/api/banda'
-const bandaService = new BandaService()
-export default {
-  data() {
-    return {
-      imagemFixa: require('@/assets/images/angra.jpg'),
-      bandas: [],
-    };
-  },
-  async created() {
-    this.bandas = await bandaService.buscarBandas()
-  }
-};
+  import BandaService from "@/api/banda";
+  const bandaService = new BandaService();
+  export default {
+    data() {
+      return {
+        imagemFixa: require("@/assets/images/angra.jpg"),
+        bandas: [],
+      };
+    },
+    async created() {
+      this.bandas = await bandaService.buscarBandas();
+    },
+  };
 </script>
 
-<style>
-</style>
+<style></style>
