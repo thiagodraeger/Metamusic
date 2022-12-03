92d6292174928b0bbc82fa3407bcf35001dbc9df
thiagodraeger <thiagodraeger123@gmail.com>
2022-12-03 16:33:04 -0300

    background animado teste

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -68,23 +68,22 @@ export default {
 
 <style scoped>
 main {
-  /* background-image: url('https://wallpaperaccess.com/full/1162700.jpg'); */
   background-color: #171717;
   background-size: cover;
   background-attachment: fixed;
 }
 
-/* body{
+/* main{
 	margin:0;
 	padding:0;
 	font-family:"arial",heletica,sans-serif;
 	font-size:12px;
-    background: #2980b9 url('https://static.tumblr.com/03fbbc566b081016810402488936fbae/pqpk3dn/MRSmlzpj3/tumblr_static_bg3.png') repeat 0 0;
-	-webkit-animation: 10s linear 0s normal none infinite animate;
-	-moz-animation: 10s linear 0s normal none infinite animate;
-	-ms-animation: 10s linear 0s normal none infinite animate;
-	-o-animation: 10s linear 0s normal none infinite animate;
-	animation: 10s linear 0s normal none infinite animate;
+    background: #2f3336 url('https://static.tumblr.com/03fbbc566b081016810402488936fbae/pqpk3dn/MRSmlzpj3/tumblr_static_bg3.png') repeat 0 0;
+	-webkit-animation: 15s linear 0s normal none infinite animate;
+	-moz-animation: 15s linear 0s normal none infinite animate;
+	-ms-animation: 15s linear 0s normal none infinite animate;
+	-o-animation: 15s linear 0s normal none infinite animate;
+	animation: 15s linear 0s normal none infinite animate;
  
 }
  
